10b39087abfe02dd142ccf18ccda1e1e8001741d
Artem <artchzhen@gmail.com>
2022-12-13 21:31:59 +0500

    recomends the app

--- New Project1/DetailViewController.swift
+++ New Project1/DetailViewController.swift
@@ -16,37 +16,33 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        title = selectedImage - создает заголовок в виде nssl038
-            title = "This image is \(selectedPictureNumber) out of \(totalPictures)"
-            
-            navigationItem.largeTitleDisplayMode = .never
-            
-            
-            if let imageToLoad = selectedImage {
-                imageView.image  = UIImage(named: imageToLoad)
-            }
+        //        title = selectedImage - создает заголовок в виде nssl038
+        title = "This image is \(selectedPictureNumber) out of \(totalPictures)"
+        
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareLink))
+        
+        
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
         
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            navigationController?.hidesBarsOnTap = true
-        }
-        
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
-            navigationController?.hidesBarsOnTap = false
-        }
-        
-        
-        
-        /*
-         // MARK: - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-         }
-         */
-        
+    }
+    @objc func shareLink() {
+        let link = "https://github.com/Busido14/project1"
+    
+        let vc = UIActivityViewController(activityItems: [link], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
